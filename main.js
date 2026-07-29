ef9b570df6f1c6be21ac1bc138420c0cb576369e
asmae <140075741+asmaeaouassar20@users.noreply.github.com>
2026-07-29 01:42:50 +0100

    get elements by id

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
+let title = document.getElementById('title');
+let price = document.getElementById('price');
+let taxes = document.getElementById('taxes');
+let ads = document.getElementById('ads');
+let discount = document.getElementById('discount');
+let total = document.getElementById('total');
+let count = document.getElementById('count');
+let category = document.getElementById('category');
+let submit = document.getElementById('submit');
+
+
+
 // get total
 // create product
 // save product in localstorage
