34fe39700e602a6dd3b589c874f31af994a13676
asmae <140075741+asmaeaouassar20@users.noreply.github.com>
2026-07-29 13:20:17 +0100

    implement main functionalities

--- main.js
+++ main.js
@@ -8,15 +8,255 @@ let count = document.getElementById('count');
 let category = document.getElementById('category');
 let submit = document.getElementById('submit');
 
+let mode = 'create';
+let indexOfProductToUpdate; //varibale global
+let searchBy = 'title';
+let lightMode = true;
+
+
+let products = [];
+getDataFromLocalStorage();
+
+function getDataFromLocalStorage() {
+    if (localStorage.products != null) {
+        products = JSON.parse(localStorage.products);
+    }
+    if(localStorage.lightMode != null){
+        lightMode = localStorage.lightMode;        
+    }
+    manageLightDarkMode();
+    showProducts()
+}
+
+function manageLightDarkMode(){
+    if (lightMode) {
+        document.getElementById('modeDisplay').innerText = 'dark mode'
+         document.querySelectorAll(".elmMode").forEach(elm => {
+            elm.classList.remove("dark")
+        })
+         document.querySelectorAll("input").forEach(elm => {
+            elm.classList.remove("dark")
+        })
+         document.querySelectorAll("button").forEach(elm => {
+            elm.classList.remove("dark")
+        })
+    } else {
+        document.getElementById('modeDisplay').innerText = 'light mode'
+
+        document.querySelectorAll(".elmMode").forEach(elm => {
+            elm.classList.add("dark")
+        })
+         document.querySelectorAll("input").forEach(elm => {
+            elm.classList.add("dark")
+        })
+         document.querySelectorAll("button").forEach(elm => {
+            elm.classList.add("dark")
+        })      
+    }
+}
+function switchLightDarkMode(){
+   lightMode = !lightMode;
+   localStorage.setItem('lightMode',lightMode);
+   manageLightDarkMode();
+}
+
+
+
+function displayProducts(productsTable) {
+    let tbody = '';
+    for (let i = 0; i < productsTable.length; i++) {
+        tbody += `
+                <tr>
+                    <td>${i + 1}</td>
+                    <td>${productsTable[i].title}</td>
+                    <td>${productsTable[i].price}</td>
+                    <td>${productsTable[i].taxes}</td>
+                    <td>${productsTable[i].ads}</td>
+                    <td>${productsTable[i].discount}</td>
+                    <td>${productsTable[i].count}</td>
+                    <td>${productsTable[i].category}</td>
+                    <td><button onclick="prefillInputsToUpdate(${i})" >update</button></td>
+                    <td><button onclick="deleteProductByIndex(${i})" >delete</button></td>
+                </tr>
+                `;
+    }
+    document.getElementById('tbody').innerHTML = tbody;
+}
 
 
 // get total
-// create product
-// save product in localstorage
+function getTotal() {
+    if (price.value != '') {
+        let result = (+price.value + +taxes.value + +ads.value) - discount.value;
+        total.innerHTML = result;
+        total.style.backgroundColor = 'rgb(146, 202, 146)';
+    } else {
+        total.innerHTML = '';
+        total.style.backgroundColor = '#eeb9b5';
+    }
+}
+
+
+// create/update product
+function createProduct(newProduct) {
+    if (newProduct.count > 1) {
+        for (let i = 0; i < newProduct.count; i++) {
+            products.push(newProduct);
+        }
+    } else {
+        products.push(newProduct);
+    }
+}
+function prefillInputsToUpdate(i) {
+    mode = 'update';
+    indexOfProductToUpdate = i; // sauvegrader globalement l'indice du profuit à modifier
+    title.value = products[i].title;
+    price.value = products[i].price;
+    taxes.value = products[i].taxes;
+    ads.value = products[i].ads;
+    discount.value = products[i].discount;
+    category.value = products[i].category;
+    getTotal();
+    count.style.display = 'none';
+    submit.innerText = 'update';
+    scroll({
+        top: 0,
+        behavior: 'smooth'
+    })
+}
+function updateProduct(newProduct) {
+    products[indexOfProductToUpdate] = newProduct;
+    mode = 'create';
+    submit.innerText = 'create';
+    count.style.display = 'block';
+}
+submit.onclick = function () {
+    let product = {
+        title: title.value.toLowerCase(),
+        price: price.value,
+        taxes: taxes.value,
+        ads: ads.value,
+        discount: discount.value,
+        total: total.innerText,
+        count: count.value,
+        category: category.value.toLowerCase()
+    }
+    if (cleanData(product)) {
+        if (mode === 'create') {
+            createProduct(product);
+        } else if (mode === 'update') {
+            updateProduct(product);
+        }
+        localStorage.setItem('products', JSON.stringify(products));
+        clearInputs();
+        showProducts();
+    }
+}
+
+
+
+
 // clear inputs
+function clearInputs() {
+    title.value = '';
+    price.value = '';
+    taxes.value = '';
+    ads.value = '';
+    discount.value = '';
+    total.innerText = '';
+    count.value = '';
+    category.value = '';
+    total.style.backgroundColor = '#eeb9b5';
+}
+
+
+
 // read
-// count
+function showProducts() {
+    getTotal()
+    displayProducts(products);
+    if (products.length > 0) {
+        document.getElementById('deleteAllSection').style.display = 'block';
+    } else {
+        document.getElementById('deleteAllSection').style.display = 'none';
+    }
+    if (document.getElementById('totalProducts')) {
+        document.getElementById('totalProducts').innerText = `(${products.length})`;
+    }
+}
+
+
+
+
+
+
+
+
 // delete
-// update
+function deleteProductByIndex(index) {
+    products.splice(i, 1);
+    localStorage.products = JSON.stringify(products);
+    showProducts();
+}
+function deleteAll() {
+    localStorage.clear();
+    products.splice(0);
+    showProducts();
+}
+
+
+
+
+
+
+
 // search
+function changeSearchBy(searchByKeyWord) {
+    searchBy = searchByKeyWord;
+    const inputSearchElm = document.getElementById('search');
+    inputSearchElm.style.display = "block";
+    inputSearchElm.placeholder = "search by " + searchBy;
+    inputSearchElm.focus();
+    // inputSearchElm.value='';
+}
+
+function searchByTitle(value) {
+    let result = [];
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].title.includes(value.toLowerCase())) {
+            result.push(products[i])
+        }
+    }
+    displayProducts(result);
+}
+function searchByCategory(value) {
+    let result = [];
+    for (let i = 0; i < products.length; i++) {
+        if (products[i].category.includes(value.toLowerCase())) {
+            result.push(products[i])
+        }
+    }
+    displayProducts(result);
+}
+function searchInProducts(value) {
+    if (searchBy === 'title') {
+        searchByTitle(value)
+    }
+    if (searchBy === 'category') {
+        searchByCategory(value);
+    }
+}
+
+
 // clean data
+function cleanData(product) {
+    return (
+        product.title != ''
+        && product.price != ''
+        && product.category != ''
+        && product.count < 100
+    )
+}
+
+// save products in localstorage
+// count
